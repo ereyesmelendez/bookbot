fba901c73c3b008d2467e1f730c10928f6d1c45e
ereyesmelendez <ereyesmelendez35@gmail.com>
2024-05-01 01:01:20 -0400

    update

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("hello world")
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    print(f"{num_words} words found in the document")
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+
+def get_book_text(path):
+    with open("/home/edrick/workspace/github.com/ereyesmelendez/bookbot/books/frankenstein.txt") as f:
+        return f.read()
+
+if __name__ == '__main__':
+    main()
+
+
+main()
